0ef29c8a8929705617aa00dfb9a33e40a3a34a56
fangyuxi <xcoder.fang@gmail.com>
2016-04-05 20:06:41 +0800

    Initial Commit of Library

---
+++ Example/Pods/Target Support Files/HYCache/HYCache-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double HYCacheVersionNumber;
+FOUNDATION_EXPORT const unsigned char HYCacheVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-HYCache_Tests/Pods-HYCache_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_HYCache_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_HYCache_TestsVersionString[];
+
