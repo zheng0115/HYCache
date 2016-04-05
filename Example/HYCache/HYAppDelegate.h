152e1f363edca3f80bf278e2cde26603987407f8
fangyuxi <xcoder.fang@gmail.com>
2016-04-05 19:57:11 +0800

    Initial commit

---
+++ Example/HYCache/HYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HYAppDelegate.h
+//  HYCache
+//
+//  Created by fangyuxi on 04/05/2016.
+//  Copyright (c) 2016 fangyuxi. All rights reserved.
+//
+
+@import UIKit;
+
+@interface HYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/HYCache/HYViewController.h
@@ -0,0 +1,13 @@
+//
+//  HYViewController.h
+//  HYCache
+//
+//  Created by fangyuxi on 04/05/2016.
+//  Copyright (c) 2016 fangyuxi. All rights reserved.
+//
+
+@import UIKit;
+
+@interface HYViewController : UIViewController
+
+@end
